a2a10acc3698e27ba80b3a176fd5e6c7f04824f7
chenxu <849237780@qq.com>
2024-03-20 16:37:37 +0100

    update

--- temp.py
+++ temp.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 # The DAG object to instantiate a DAG
 from airflow import DAG 
 # Operators used to write tasks
-from airflow.Operators.bash_operator import Bash_Operator
+from airflow.operators.bash_operator import Bash_Operator
 # utils make scheduling easy
 from airflow.utils.dates import days_ago
 
@@ -49,6 +49,51 @@ unzip_data = Bash_Operator(
 # define a task to extract data from csv file
 extract_data_from_csv = Bash_Operator(
     task_id = 'extract_data_from_csv',
-    bash_commandd = 'cut -dsou'
+    bash_command = 'cut -d -f1-4 /home/project/airflow/dags/finalassignment/vehicle-data.csv > \
+        /home/project/airflow/dags/finalassignment/csv_data.csv',
+    dag = dag,
 )
 
+
+# define a task to extact data from tsv file
+
+extract_data_from_tsv = Bash_Operator(
+    task_id = 'extract_data_from_tsv',
+    bash_command = 'cut -d -f5-7 /home/project/airflow/dags/finalassignment/tollplaza-data.tsv > \
+        /home/project/airflow/dags/finalassignment/tsv_data.csv',
+    dag = dag,
+)
+
+# define a task to extract data from fixed width txt file
+
+extract_data_from_fixed_width = Bash_Operator(
+    task_id = 'extract_data_from_fixed_width',
+    bash_command = 'cut -c59-67 /home/project/airflow/dags/finalassignment/payment-data.txt > \
+        /home/project/airflow/dags/finalassignment/fixed_width_data.csv',
+    dag = dag,
+)
+
+# define a task to consolidate data extracted from previous tasks
+
+consolidate_data = Bash_Operator(
+    task_id = 'consolidate_data',
+    bash_command = 'paste /home/project/airflow/dags/finalassignment/csv_data.csv \
+        /home/project/airflow/dags/finalassignment/tsv_data.csv \
+        /home/project/airflow/dags/finalassignment/fixed_width_data.csv \
+        > /home/project/airflow/dags/finalassignment/extracted_data.csv',
+    dag = dag,
+)
+
+# Transform and load data
+
+transform_data = Bash_Operator(
+    task_id = 'transform_data',
+    bash_command = 'tr "[a-z]" "[A-Z]" < /home/project/airflow/dags/finalassignment/extracted_data.csv \
+        > /home/project/airflow/dags/finalassignment/transformed_data.csv',
+    dag = dag,
+)
+
+# Define the task pipeline
+
+unzip_data >> extract_data_from_csv >> extract_data_from_tsv >> extract_data_from_fixed_width \
+    >> consolidate_data >> transform_data
